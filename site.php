637c5cacab318bc18e88e1cd18fab77f7ed2e2de
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-21 06:15:24 -0500

    This is my first commit

---
+++ site.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is my php project</title>
+</head>
+<body>
+    <?php
+    $characterName = "Gabroel";
+    $characterAge = 29;
+
+    echo"There once was a man named $characterName <br>";
+    echo "he was $characterAge years old <br>";
+    $characterName = "batisseur fono";
+    $characterAge = 32;
+    echo "he really liked the name $characterName <br>";
+     echo "was $characterAge years old <br>";
+    echo "nuy didn't like bieng $characterAge <br>";
+    ?>
+
+</body>
+</html>
