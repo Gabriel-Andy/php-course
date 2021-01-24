8c3adb68fdfcfa30abccbce8855e3629d9af6876
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-24 10:40:38 -0500

    I learnt how to use the include in php

--- site.php
+++ site.php
@@ -6,16 +6,13 @@
     <title>This is my php project</title>
 </head>
 <body>
-<h2>understand while loop</h2>
+<?php 
+$title = "my first post";
+$author = "Gabriel Fono";
+$wordCount = 400;
+include "article-header.php"
+?>
 
-
-
-    <?php
-    $number = array(25,35,23,22,55,23,24);
-    for($i = 0; $i < count($number); $i++ ) {
-      echo("this is number $number[$i] <br>");
-    }
-     ?>
 
 </body>
 </html>
