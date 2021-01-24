5b3532c3c35d93d35f7c6c5b6dda90a1c0209323
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-24 04:35:03 -0500

    I learnt about array datastructure

--- site.php
+++ site.php
@@ -6,14 +6,14 @@
     <title>This is my php project</title>
 </head>
 <body>
-  
-    <form action = "site.php" method = "post">
-    password: <input type = "password" name = "password"><br>
-    <input type = "submit"/>
-    </form>
+  <h1> understand array in php</h1>
     <br>
     <?php
-     echo $_POST["password"]
+    $friends = array('Tabongue', "Yannick", "fono");
+    $friends[1]= "Zoubar la racaille";
+    $friends[3] = "Takis";
+    
+    echo count($friends)
       ?>
 
 </body>
