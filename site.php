f0e329eb0ed393cff8ddbb4edff03fa001aab233
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-24 08:33:43 -0500

    I learn the differnce between dowhile loop and while loop in php

--- site.php
+++ site.php
@@ -6,39 +6,20 @@
     <title>This is my php project</title>
 </head>
 <body>
-<h2>this is my calulator</h2>
-<form action = "site.php" method = "post">
-num1: <input type = "number" step = "0.001"   name = "num1" /> <br>
-operator: <input type = "text"  name = "operator" /><br>
-num2: <input type = "number" name = "num2" /><br>
-<input type = "submit"/>
-</form>
+<h2>understand while loop</h2>
+
+
 
     <?php
-    $num1 = $_POST['num1'];
-     $num2 = $_POST['num2'];
-     $operator = $_POST["operator"];
-   function output($operator,$num1,$num2) {
-     switch($operator) {
-       case "+" :
-       echo $num1 + $num2;
-       break;
-       case "-" :
-       echo $num1 - $num2;
-       break;
-      case "/" :
-       echo $num1 / $num2;
-       break;
-       case "*" :
-       echo $num1 * $num2;
-       break;
-       default :
-       echo "invalid operator";
+     $index = 1;
+     do {
+       echo " I am number $index <br>";
+       $index++;
+       if($index == 6) {
+         echo "I love this number";
+       }
      }
-      
-  }
-  output($operator,$num1,$num2)
-       
+     while($index < 6) 
      ?>
 
 </body>
