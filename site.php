50c0741e1b728fdbb42c1c1fe692e18f860c914c
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-01-24 07:47:28 -0500

    I built a simple calulator

--- site.php
+++ site.php
@@ -6,14 +6,32 @@
     <title>This is my php project</title>
 </head>
 <body>
+<h2>this is my calulator</h2>
+<form action = "site.php" method = "post">
+num1: <input type = "number"  value = 2 name = "num1" />
+operator: <input type = "text" value = "+" name = "operator" />
+num2: <input type = "number" name = "num2" />
+<input type = "submit"/>
+</form>
 
     <?php
-    function sayHi($name){
-      echo "hello $name <br> ";
-    }
-    sayHi("gabriel");
-    sayHi("Tabongue");
-    sayHi("batisseur")
+    $num1 = $_POST['num1'];
+     $num2 = $_POST['num2'];
+     $operator = $_POST["operator"];
+     if($operator == "+") {
+        echo $num1 + $num2;
+     }
+     elseif($operator == "-"){
+       echo $num1 - $num2;
+     }
+     elseif($operator == "/") {
+       echo $num1 / $num2;
+     }elseif($operator == "*") {
+        echo $num1 * $num2;
+     } else {
+       echo "invalid operator";
+     }
+       
      ?>
 
 </body>
